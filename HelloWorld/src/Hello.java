8444afe5d1bad42c2f9313d43aa91e732db050b3
ruchipareek5 <ruchipareek5@gmail.com>
2018-06-13 15:06:24 +0530

    Author                     :Ruchi Pareek Description                :Practice Hello world and Parse XML program

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,11 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		int myFirstNumber =10;
+		System.out.println(myFirstNumber);
+
+	}
+
+}
